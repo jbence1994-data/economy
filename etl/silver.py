34ade5847c159bfc393c8e5bae04cc0d7bef51df
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2026-02-12 11:01:48 +0100

    Silver ETL

---
+++ etl/silver.py
@@ -0,0 +1,36 @@
+from os import environ as env
+
+from pyspark.sql import SparkSession
+
+
+def silver_etl():
+    spark = (SparkSession.builder
+             .master("local[*]")
+             .appName("economy_silver")
+             .getOrCreate())
+
+    inflation_silver_data_frame = (spark
+                                   .read
+                                   .parquet(env.get("INFLATION_MEDALLION_BRONZE")))
+
+    interest_rate_silver_data_frame = (spark
+                                       .read
+                                       .parquet(env.get("INTEREST_RATE_MEDALLION_BRONZE")))
+
+    (inflation_silver_data_frame
+     .write
+     .mode("overwrite")
+     .partitionBy("year")
+     .parquet(env.get("INFLATION_MEDALLION_SILVER")))
+
+    (interest_rate_silver_data_frame
+     .write
+     .mode("overwrite")
+     .partitionBy("year")
+     .parquet(env.get("INTEREST_RATE_MEDALLION_SILVER")))
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    silver_etl()
